c214f5b9096dc7425496ab41ef165a750d24e5e0
Kai Xiang <anythinnk@live.com>
2020-05-11 04:24:32 +0800

    Added a theme toggle, and grouped up JS operations into functions.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const calcEle = document.querySelector('#calculator');
-const histBtn = document.querySelector('#history-btn');
-const histCloseBtn = document.querySelector('#close-history-btn');
+function toggleDarkMode() {
+    const themeLightText = document.querySelector('#theme-light-label');
+    const themeDarkText = document.querySelector('#theme-dark-label');
+    document.body.classList.toggle('dark-mode');
+    themeLightText.classList.toggle('bold-label');
+    themeDarkText.classList.toggle('bold-label');
+}
 
-histBtn.addEventListener('click', () => {
-    calcEle.classList.toggle('history');
-})
+function enableButtons() {
+    const themeToggle = document.querySelector('#toggle-theme');
+    themeToggle.addEventListener('click', () => toggleDarkMode());
 
-histCloseBtn.addEventListener('click', () => {
-    if(calcEle.classList.contains('history')) {
+    const calcEle = document.querySelector('#calculator');
+    const histBtn = document.querySelector('#history-btn');
+    histBtn.addEventListener('click', () => {
         calcEle.classList.toggle('history');
-    }
-})
+    })
+    
+    const histCloseBtn = document.querySelector('#close-history-btn');
+    histCloseBtn.addEventListener('click', () => {
+        if(calcEle.classList.contains('history')) {
+            calcEle.classList.toggle('history');
+        }
+    })
+}
+
+function load() {
+    enableButtons();
+}
+
+load();
