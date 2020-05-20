b9ff41b2929b026aca19f24cedf7c9fe7759c047
Kai Xiang <anythinnk@live.com>
2020-05-20 16:09:28 +0800

    Got the non-calculation buttons working (0-9, period, backspace, CE, C, negative toggle).

--- script.js
+++ script.js
@@ -1,3 +1,96 @@
+const recordEle = document.querySelector('#record');
+const displayEle = document.querySelector('#display');
+const validKeys = [ '%',    'Delete',   'Escape',   'Backspace', 
+                    'r',    'q',        '@',        '/',
+                    '7',    '8',        '9',        '*',
+                    '4',    '5',        '6',        '-',
+                    '1',    '2',        '3',        '+',
+                    'F9',   '0',        '.',        '=', 'Enter']
+const immediateFuncs = ['percent', 'inverse', 'squared', 'sqrt'];
+const calcOnNextFuncs = ['divide', 'multiply', 'subtract', 'add'];
+
+let currArr; // array for calcs
+let recordArr; // array of strings to join for display
+
+function percent(a, b) {
+    return a*b/100;
+}
+
+function clearEntry() {
+    displayEle.textContent = '0';
+}
+
+function clearAll() {
+    clearEntry();
+    recordEle.textContent = '';
+}
+
+function backspace() {
+    let currText = displayEle.textContent;
+    if (currText.length > 1) {
+        displayEle.textContent = currText.substring(0, currText.length - 1);
+    } else {
+        displayEle.textContent = '0';
+    }
+}
+
+function inverse(a) {
+    if(a != 0) {
+        return 1/a;
+    } else {
+        return "Cannot divide by zero";
+    }
+}
+
+function squared(a) {
+    return a*a;
+}
+
+function sqrt(a) {
+    if (a >= 0) {
+        return Math.sqrt(a);
+    } else {
+        return "Invalid input";
+    }
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function add(a, b) {
+    return a+b;
+}
+
+function negative() {
+    let currText = displayEle.textContent;
+    if (currText[0] === '-') {
+        return currText.substring(1, currText.length);
+    } else {
+        return `-${currText}`;
+    }
+}
+
+function numberInput(str) {
+    let currText = displayEle.textContent;
+    if (currText === '0') {
+        if (str !== '.') {
+            currText = str;
+        }
+    } else if (!(currText[currText.length - 1] === '.' && str === '.')) {
+        currText += str;
+    }
+    displayEle.textContent = currText;
+}
+
 function toggleDarkMode() {
     const themeLightText = document.querySelector('#theme-light-label');
     const themeDarkText = document.querySelector('#theme-dark-label');
@@ -21,6 +114,25 @@ function enableButtons() {
         if(calcEle.classList.contains('history')) {
             calcEle.classList.toggle('history');
         }
+    })
+
+    const clearAllBtn = document.querySelector('#clear-all');
+    clearAllBtn.addEventListener('click', () => clearAll());
+
+    const clearEntryBtn = document.querySelector('#clear-entry');
+    clearEntryBtn.addEventListener('click', () => clearEntry());
+
+    const backspaceBtn = document.querySelector('#backspace');
+    backspaceBtn.addEventListener('click', () => backspace());
+
+    const numberBtns = document.querySelectorAll('.number');
+    numberBtns.forEach(btn => {
+        btn.addEventListener('click', () => numberInput(btn.textContent));
+    })
+
+    const negativeBtn = document.querySelector('#negative');
+    negativeBtn.addEventListener('click', () => {
+        displayEle.textContent = negative(displayEle.textContent);
     })
 }
 
