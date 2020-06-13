e9192df48a08120855dd220be52c738054bf83aa
Kai Xiang <anythinnk@live.com>
2020-06-13 14:49:17 +0800

    Overhauled the script file as the old one had too much "patchwork" to add functionality. Still have to do 'equals' and 'history' functions to catch up in functionality.

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 const calcEle = document.querySelector('#calculator');
 const recordEle = document.querySelector('#record');
 const displayEle = document.querySelector('#display');
-const immediateFuncs = ['percent', 'inverse', 'squared', 'sqrt'];
-const calcOnNextFuncs = ['divide', 'multiply', 'subtract', 'add', 'equals'];
+const immediateFuncs = ['percent', 'inverse', 'squared', 'sqrt', 'negative'];
+const calcOnNextFuncs = ['divide', 'multiply', 'subtract', 'add'];
 const identifierSymbol = {
     'divide': '\u00f7',
     'multiply': '\u00d7',
@@ -14,6 +14,7 @@ const identifierSymbol = {
     'inverse': '1/',
     'squared': 'sqr',
     'sqrt': '\u221a',
+    'negative': 'negate',
     'equals': '\u003d'
 }
 const validKeysToID = {
@@ -44,37 +45,131 @@ const validKeysToID = {
     'Enter': 'equals'
 }
 
-let prevOper; // prev operator for calcs on currArr
-let currArr; // array for calcs
-let immStr; // string containing numbers with immediate calcs done on them e.g. 'sqr(9)' for 9 squared, needed as immediate calcs are not confirmed to be used until a calcOnNextFunc function is done
-let recordArr; // array of strings to join for display
-let displayIsResult;
+let prevOper = null,
+    tempLastInput = false,
+    calcArr = [], // array for calcs
+    recordArr = [], // array to join for record display, numbers rounded to specific dp
+    prevCalcResult = null, // unrounded
+    dispCalcResult = null, // unrounded, will be rounded when converted to textContent
+    displayIsResult = false;
+
+// immediate operators operate on the display number if NOT displayIsResult OR if displayIsResult but last element of recordArr is a delayed operator, and on the last value of calcArr if displayIsResult but last element is not a delayed operator
+
+// once a delayed operator is pressed, any prev delayed operator is evaluated and any immediate operator is operated on the shown result
+// once a number is pressed, any immediate operator is operated on the shown number
+// once an immediate operator is pressed, it is operated on the last number
+// for records that end with equals (i.e. frm history), any immediate operator is operated on the last number
+
+// DRAFT PROCESS
+// so basically, on pressing any number
+    // number input (old code should work, but need to edit negative to work in the same way as an immediate operator (takes in a number and returns another)
+    // if displayIsResult
+        // if tempLastInput (history or imm oper), clearEntry and clearLastInput
+        // else (equals button or delayed oper), clearAll if equals, clearEntry O.W.
+
+// on pressing any delayed operator, 
+    // if NOT displayIsResult (number input)
+        // append display number to calcArr
+        // if more than 2 numbers, replace first 2 with dispCalcResult
+        // if dispCalcResult is not null, update prevCalcResult with dispCalcResult
+        // calculate if calcArr is not empty (return only elem of calcArr if <2 elems)
+        // update dispCalcResult, display it
+        // displayIsResult becomes true
+    // else if displayIsResult
+        // if last element of recordArr is a delayed operator or equals
+            // pop the last element of recordArr
+        // if not 
+            // (appending done by the first press of immediate operator)
+            // if dispCalcResult is not null, update prevCalcResult with dispCalcResult
+            // calculate if calcArr is not empty (return only elem of calcArr if <2 elems)
+            // update dispCalcResult, display it
+    // update prevOper with the delayed operator
+    // tempLastInput becomes false
+    // update recordArr
+
+// on pressing any immediate operator
+    // if NOT displayIsResult OR if displayIsResult AND last element of recordArr is a delayed operator (number pressed or delayed operator pressed)
+        // if NOT displayIsResult AND operator is "negative"
+            // do negative function on display number
+            // return
+        // append (display number if NOT displayIsResult, dispCalcResult O.W) to calcArr
+        // if more than 2 numbers, replace first 2 with dispCalcResult
+        // update recordArr using last elem of calcArr (except percent)
+    // else if displayIsResult AND last element of recordArr is NOT a delayed operator (equals or prev imm operator)
+        // if last elem of recordArr is "="
+            // pop the last elem of recordArr
+        // update recordArr using last elem of recordArr (except percent)
+    // displayIsResult becomes true
+    // tempLastInput becomes true
+    // calculate on the last element (both for percent, 0 as a if only 1 element) of calcArr and replace the element after calc
+    // if percent was done, update recordArr using last elem of calcArr as well
+
+// equals button
+    // if NOT displayIsResult OR if displayIsResult AND last element of recordArr is a delayed operator (number pressed or delayed operator pressed)
+        // append (display number if NOT displayIsResult, dispCalcResult O.W) to calcArr
+        // if more than 2 numbers, replace first 2 with dispCalcResult
+        // if dispCalcResult is not null, update prevCalcResult with dispCalcResult
+        // calculate if calcArr is not empty (return only elem of calcArr if <2 elems)
+        // update dispCalcResult, display it
+        // displayIsResult becomes true
+    // else if displayIsResult
+        // if last element of recordArr is equals (from history or repeating equals)
+            // pop the last elem of recordArr
+            // if dispCalcResult is not null
+                // update prevCalcResult with dispCalcResult
+                // calcArr = [dispCalcResult, last elem of calcArr]
+                // recordArr = [dispCalcResult, last 2 elem of recordArr]
+            // calculate if calcArr is not empty (return only elem of calcArr if <2 elems)
+            // update dispCalcResult, display it
+        // if not 
+            // (appending done by the first press of immediate operator)
+            // if dispCalcResult is not null, update prevCalcResult with dispCalcResult
+            // calculate if calcArr is not empty (return only elem of calcArr if <2 elems)
+            // update dispCalcResult, display it
+    // update recordArr
+    // tempLastInput becomes false
+    // save prevOper, prevCalcResult, dispCalcResult to html dataset
+    // save record textContent and display textContent to history displays
 
 function resetVars() {
     prevOper = null;
-    currArr = [];
-    immStr = null;
+    tempLastInput = false;
+    calcArr = [];
     recordArr = [];
+    prevCalcResult = null;
+    dispCalcResult = null;
     displayIsResult = false;
 }
 
-function percent(a, b) {
-    return a*b/100;
+function logVars() {
+    let tempObj = {prevOper, tempLastInput, calcArr, recordArr, prevCalcResult, dispCalcResult, displayIsResult};
+    console.log(tempObj);
 }
 
 function clearEntry() {
-    if (calcEle.classList.contains('only-numbers') || prevOper === 'equals') {
+    if (calcEle.classList.contains('only-numbers')) {
         clearAll();
+    } else if (tempLastInput) {
+        clearLastInput();
     }
     displayEle.textContent = '0';
-    if (immStr !== null) {
-        immStr = null;
+    displayIsResult = false;
+}
+
+function clearLastInput() {
+    if (lastElemOf(recordArr) === identifierSymbol['equals']) {
+        recordArr.pop();
+    }
+    recordArr.pop();
+    calcArr.pop();
+
+    if (recordArr.length > 0) {
         displayRecord(recordArr);
     }
 }
 
 function clearAll() {
-    if (calcEle.classList.contains('only-numbers')) { 
+    if (calcEle.classList.contains('only-numbers')) {
         enableOperators();
     }
     resetVars();
@@ -95,6 +190,10 @@ function backspace() {
             displayEle.textContent = '0';
         }
     }
+}
+
+function percent(a, b) {
+    return a*b/100;
 }
 
 function inverse(a) {
@@ -125,24 +224,20 @@ function add(a, b) {
     return a+b;
 }
 
-function negative() {
-    let currText = displayEle.textContent;
-    if (currText[0] === '-') {
-        return currText.substring(1, currText.length);
-    } else {
-        return `-${currText}`;
-    }
+function negative(a) {
+    return -a;
 }
 
 function numberInput(str) {
     if (calcEle.classList.contains('only-numbers')) {
         clearAll();
     } else if (displayIsResult) {
-        if (prevOper === 'equals') {
+        if (tempLastInput) {
+            clearEntry();
+        } else if (lastElemOf(recordArr) === identifierSymbol['equals']) {
             clearAll();
         } else {
             clearEntry();
-            displayIsResult = false;
         }
     }
     let currText = displayEle.textContent;
@@ -158,96 +253,115 @@ function numberInput(str) {
     displayEle.textContent = currText;
 }
 
-function evalPrevOper(newNum, operator) {
-    if (!displayIsResult || immStr !== null) {
-        currArr.push(newNum);
-        recordArr.push((immStr !== null) ? immStr : newNum);
-        if (prevOper !== null) {
-            let result;
-            switch (operator) {
-                case 'divide':
-                    result = divide(...currArr);
-                    break;
-                case 'multiply':
-                    result = multiply(...currArr);
-                    break;
-                case 'subtract':
-                    result = subtract(...currArr);
-                    break;
-                case 'add':
-                    result = add(...currArr);
-                    break;
-                case 'equals':
-                    result = newNum;
-            }
-            currArr = [result];
-            displayEle.textContent = round(checkValidity(result));
+function prevOperResult(operator) {
+    let result;
+    if (calcArr.length === 2) {
+        switch(operator) {
+            case 'divide':
+                result = divide(...calcArr);
+                break;
+            case 'multiply':
+                result = multiply(...calcArr);
+                break;
+            case 'subtract':
+                result = subtract(...calcArr);
+                break;
+            case 'add':
+                result = add(...calcArr);
         }
-        displayIsResult = true;
-    } else if (prevOper === 'equals') {
-        recordArr = [newNum];
-    } else {
-        recordArr.pop();
+    } else if (calcArr.length === 1) {
+        result = calcArr[0];
     }
+    return result;
+}
+
+function evalPrevOper(newNum, operator) {
+    if (!displayIsResult) {
+        recordArr.push(round(newNum));
+        calcArr.push(newNum);
+        calcArr = (calcArr.length > 2) ? [dispCalcResult, lastElemOf(calcArr)] : calcArr;
+        prevCalcResult = (dispCalcResult) ? dispCalcResult : null;        
+        dispCalcResult = prevOperResult(operator);
+        displayEle.textContent = round(checkValidity(dispCalcResult));
+        displayIsResult = true;
+    } else {
+        if ([...calcOnNextFuncs.map((identifier) => identifierSymbol[identifier]), identifierSymbol['equals']].includes(lastElemOf(recordArr))) {
+            recordArr.pop();
+        } else {
+            prevCalcResult = (dispCalcResult) ? dispCalcResult : null;
+            dispCalcResult = prevOperResult(operator);
+            displayEle.textContent = round(checkValidity(dispCalcResult));
+        }
+    }
+}
+
+function immOperResult(operator) {
+    let result;
+    let num = lastElemOf(calcArr);
+    switch (operator) {
+        case 'percent':
+            result = (calcArr.length === 1) ? 0 : percent(...calcArr);
+            break;
+        case 'inverse':
+            result = inverse(num);
+            break;
+        case 'squared':
+            result = squared(num);
+            break;
+        case 'sqrt':
+            result = sqrt(num);
+            break;
+        case 'negative':
+            result = negative(num);
+    }
+    return result;
 }
 
 function evalImmOper(newNum, operator) {
-    let result;
-    switch (operator) {
-        case 'percent':
-            result = (currArr.length === 0) ? 0 : percent(currArr[0], newNum);
-            break;
-        case 'inverse':
-            result = inverse(newNum);
-            break;
-        case 'squared':
-            result = squared(newNum);
-            break;
-        case 'sqrt':
-            result = sqrt(newNum);
+    if (!displayIsResult || displayIsResult && calcOnNextFuncs.map((identifier) => identifierSymbol[identifier]).includes(lastElemOf(recordArr))) {
+        if (!displayIsResult && operator === 'negative') {
+            displayEle.textContent = negative(newNum);
+            return;
+        }
+        recordArr.push(round(newNum));
+        calcArr.push(newNum);
+        calcArr = (calcArr.length > 2) ? [dispCalcResult, lastElemOf(calcArr)] : calcArr;
+    } else if (lastElemOf(recordArr) === identifierSymbol['equals']) {
+        recordArr.pop();
     }
-
-    if (operator === 'percent') {
-        immStr = result;
-    } else {
-        immStr = (immStr === null) ? `${identifierSymbol[operator]}(${newNum})`: `${identifierSymbol[operator]}(${immStr})`;
-    }
-    
-    displayEle.textContent = round(checkValidity(result));
+    let tempResult;
+    tempResult = immOperResult(operator);
+    calcArr.pop();
+    calcArr.push(tempResult);
+    displayEle.textContent = round(checkValidity(tempResult));
     displayIsResult = true;
+
+    let tempRecord;
+    tempRecord = (operator === 'percent') ? round(tempResult) : `${identifierSymbol[operator]}(${lastElemOf(recordArr)})`;
+    recordArr.pop();
+    recordArr.push(tempRecord);
 }
 
 function operatorInput(identifier) {
-    let currText = displayEle.textContent;
-
+    let currNum = (displayIsResult) ? dispCalcResult : Number(displayEle.textContent);
     if (calcOnNextFuncs.includes(identifier)) {
-        if (identifier === 'equals' && calcEle.classList.contains('only-numbers')) {
-            clearAll();
-        } else {
-            evalPrevOper(Number(currText), prevOper);
-            prevOper = identifier;
-            immStr = null;
-            recordArr.push(identifierSymbol[identifier]);
-            displayRecord(recordArr);
-            if (identifier === 'equals' && isNumeric(displayEle.textContent)) {
-                addToHistory();
-            }
-        }
+        evalPrevOper(currNum, prevOper);
+        prevOper = identifier;
+        tempLastInput = false;
+        recordArr.push(identifierSymbol[identifier]);
     } else if (immediateFuncs.includes(identifier)) {
-        evalImmOper(Number(currText), identifier);
-        if (prevOper === 'equals') {
-            displayRecord([immStr]);
-            recordArr = [];
-        } else {
-            let tempArr = Array.from(recordArr);
-            tempArr.push(immStr);
-            displayRecord(tempArr);
-        }
+        evalImmOper(currNum, identifier);
+        tempLastInput = true;
     }
+    displayRecord(recordArr);
+    logVars();
 }
 
+// equals func
+// history funcs
+
 function checkValidity(result) {
-    result = (result === Infinity) ? 'Overflow' : result;
+    result = (result === Infinity || result === -Infinity) ? 'Overflow' : result;
     if (isNaN(result)) {
         disableOperators();
     }
@@ -261,62 +375,8 @@ function buttonInput(e) {
     }
 }
 
-function addToHistory() {
-    const histCloseBtn = document.querySelector('#close-history-btn');
-    const historyContainer = document.querySelector('#history-list');
-    const noHistMsg = document.querySelector('#no-history-message');
-    noHistMsg.style.display = 'none';
-    
-    let historyRecord = document.createElement('div');
-    historyRecord.classList.add('history-record');
-
-    let recordEqn = document.createElement('span');
-    recordEqn.classList.add('equation');
-    recordEqn.textContent = recordEle.textContent;
-
-    let recordResult = document.createElement('span');
-    recordResult.classList.add('result');
-    recordResult.textContent = displayEle.textContent;
-
-    historyRecord.appendChild(recordEqn);
-    historyRecord.appendChild(recordResult);
-    historyRecord.addEventListener('click', () => {
-        displayHistRecord(historyRecord);
-        prevOper = null;
-        if (histCloseBtn.style.display !== 'none') {
-            histCloseBtn.click();
-        }
-    });
-
-    const delHistBtn = document.querySelector('#del-history-btn');
-    delHistBtn.style.display = 'initial';
-
-    historyContainer.prepend(historyRecord);
-    historyRecord.classList.add('faded');
-    requestAnimationFrame(() => historyRecord.classList.remove('faded'));
-}
-
-function displayHistRecord(record) {
-    let equation = record.querySelector('.equation').textContent;
-    recordEle.textContent = equation;
-    recordArr = getEquationAsArr(equation);
-
-    let result = Number(record.querySelector('.result').textContent);
-    displayEle.textContent = result;
-    currArr = [result];
-}
-
-function deleteHistory() {
-    const historyContainer = document.querySelector('#history-list');
-    const noHistMsg = document.querySelector('#no-history-message');
-    noHistMsg.style.display = 'initial';
-
-    while (historyContainer.firstChild) {
-        historyContainer.removeChild(historyContainer.lastChild);
-    }
-
-    const delHistBtn = document.querySelector('#del-history-btn');
-    delHistBtn.style.display = 'none';
+function lastElemOf(arr) {
+    return arr[arr.length-1];
 }
 
 function displayRecord(arr) {
@@ -380,11 +440,6 @@ function enableButtons() {
     numberBtns.forEach(btn => {
         btn.addEventListener('click', () => numberInput(btn.textContent));
     })
-
-    const negativeBtn = document.querySelector('#negative');
-    negativeBtn.addEventListener('click', () => {
-        displayEle.textContent = negative(displayEle.textContent);
-    })
 }
 
 function enableKeyPress() {
@@ -400,9 +455,6 @@ function disableOperators() {
         }
     });
 
-    const negativeBtn = document.querySelector('#negative');
-    negativeBtn.disabled = true;
-
     const decimalBtn = document.querySelector('#decimal');
     decimalBtn.disabled = true;
 }
@@ -411,9 +463,6 @@ function enableOperators() {
     calcEle.classList.toggle('only-numbers');
     const operatorBtns = document.querySelectorAll('.operator');
     operatorBtns.forEach(btn => btn.disabled = false);
-
-    const negativeBtn = document.querySelector('#negative');
-    negativeBtn.disabled = false;
 
     const decimalBtn = document.querySelector('#decimal');
     decimalBtn.disabled = false;
